e685d20e0951f5720cdd28c0cce8077bbb79b11d
Elizabeth <tjeudy.e@gmail.com>
2023-07-29 20:35:56 -0400

    forecast card component

--- src/components/ForecastCard.js
+++ src/components/ForecastCard.js
@@ -1,27 +1,60 @@
-const getCardColor = (aqi, forecast) => {
-	if (aqi && forecast <= 50) {
+// displays color for aqi levels
+
+const getCardColor = (aqi) => {
+	if (aqi <= 50) {
 		return "bg-success text-white";
-	} else if (aqi && forecast <= 100) {
+	} else if (aqi <= 100) {
 		return "bg-warning";
-	} else if (aqi && forecast <= 150) {
+	} else if (aqi <= 150) {
 		return "bg-orange ";
-	} else if (aqi && forecast <= 200) {
+	} else if (aqi <= 200) {
 		return "bg-danger text-white";
-	} else if (aqi && forecast <= 300) {
+	} else if (aqi <= 300) {
 		return "bg-very-unhealthy text-white";
 	} else {
 		return "bg-hazardous";
 	}
 };
 
+// forecast card displays the date, max/min, pm25(only)
+
 const ForecastCard = ({ data }) => {
-	const { aqi, city, forecast } = data;
-	const cardColor = getCardColor(aqi);
+	const { city, forecast } = data;
+	const cardColor = getCardColor(data.aqi);
+
+	function ForecastDay({ data }) {
+		// const { aqi, date, time } = data;
+		return <div>{data.date}</div>;
+	}
+
 	return (
 		<div className={`card mb-4 ${cardColor}`}>
 			<div className="card-body">
-				<h5 className="card-title">{city.name}</h5>
-				<h6 className="card-subtitle mb-2">Forecast {forecast.daily[0]}</h6>\
+				<h5 className="card-title">{city.name} 7-Day Forecast</h5>
+				<table className="table table-bordered">
+					<thead>
+						<tr>
+							<th scope="col">Date</th>
+							<th scope="col">MAX & MIN</th>
+							<th scope="col">PM25</th>
+						</tr>
+					</thead>
+					<tbody>
+						{/* maps through forecast object, pulling out data */}
+						{forecast.daily.pm25.map((pm25) => (
+							<tr key={pm25.day}>
+								<td>
+									<ForecastDay data={pm25} />
+									{pm25.day}
+								</td>
+								<td>
+									Max: {pm25.max}, Min: {pm25.min}
+								</td>
+								<td>{pm25.avg}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
 			</div>
 		</div>
 	);
