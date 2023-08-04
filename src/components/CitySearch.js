fbf1ceb24bab9e02b04350b0cbab4f7753d256c7
Elizabeth <tjeudy.e@gmail.com>
2023-08-03 21:02:02 -0400

    removed comments

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,40 +1,5 @@
 import React, { useState } from "react";
 
-/** const CitySearch = ({ getAirQuality, forecastData }) => {
-	const [inputValue, setInputValue] = useState("");
-
-	const handleInputChange = (event) => {
-		setInputValue(event.target.value);
-	};
-
-	// replaces spaces with hyphens in order to read
-	const handleSearch = (event) => {
-		event.preventDefault();
-		const formattedCity = inputValue.replace(/ /g, "-");
-		getAirQuality(formattedCity);
-	};
-
-	return (
-		<form onSubmit={handleSearch} className="mb-4">
-			<input
-				type="text"
-				placeholder="Search city here..."
-				onChange={handleInputChange}
-				className="form-control"
-			></input>
-			<button type="submit" className="btn btn-primary mt-3">
-				Search
-			</button>
-			{forecastData && (
-				<div className="mt-4">
-					<h5>Forecast Data:</h5>
-					<p>Property 1: {forecastData.daily.pm25}</p>
-				</div>
-			)}
-		</form>
-	);
-};*/
-
 const CitySearch = ({ onSearch }) => {
 	const [inputValue, setInputValue] = useState("");
 
